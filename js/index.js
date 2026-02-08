f67c58d897d66521d88a8a8d926bd01f203a0f69
Daniel <89817884+daniel-lzyn@users.noreply.github.com>
2026-02-08 10:34:57 +0800

    Created logic function to create a book card.

--- js/index.js
+++ js/index.js
@@ -15,3 +15,55 @@ function addBookToLibrary(title, author, totalPages, readStatus) {
 
 addBookToLibrary("Makanya, Mikir!", "Abigail Limuria", 295, false)
 console.log(myLibrary)
+
+function createBookCard(bookObject) {
+    // card container
+    const cardContainter = document.createElement('div');
+
+    // book title
+    const title = document.createElement('h2');
+    title.textContent = bookObject.title;
+
+    // book info
+    const bookInfoContainer = document.createElement('div');
+    bookInfoContainer.classList.add('book-info');
+
+    const authorLeftSide = document.createElement('p');
+    const authorRightSide = document.createElement('p');
+    const totalPagesLeftSide = document.createElement('p');
+    const totalPagesRightSide  = document.createElement('p');
+    const statusLeftSide = document.createElement('p');
+    const statusRightSide = document.createElement('p');
+
+    authorLeftSide.textContent = "Author:";
+    authorRightSide.textContent = bookObject.author;
+    totalPagesLeftSide.textContent = "Total Pages:";
+    totalPagesRightSide.textContent = bookObject.totalPages;
+    statusLeftSide.textContent = "Status:";
+    statusRightSide.textContent = bookObject.readStatus;
+
+    bookInfoContainer.append(
+        authorLeftSide,
+        authorRightSide,
+        totalPagesLeftSide,
+        totalPagesRightSide,
+        statusLeftSide,
+        statusRightSide,);
+
+    // buttons
+    const btnContainer = document.createElement('div');
+
+    const statusBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    statusBtn.classList.add('status_btn');
+    deleteBtn.classList.add('delete_btn');
+
+    statusBtn.textContent = "Change Status";
+    deleteBtn.textContent = "Delete Book";
+
+    btnContainer.append(statusBtn, deleteBtn);
+
+    // Append all
+    cardContainter.append(title, bookInfoContainer, btnContainer);
+}
